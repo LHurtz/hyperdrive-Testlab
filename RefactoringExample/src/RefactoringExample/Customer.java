d1dae73f76ffe37287b4afa9c6320799a35c2878
Lukas Hurtz <redhairy@live.de>
2014-05-17 12:09:15 +0200

    1. Schritt
    Berechnung des "Amount" ausgelagert

--- RefactoringExample/src/RefactoringExample/Customer.java
+++ RefactoringExample/src/RefactoringExample/Customer.java
@@ -27,7 +27,26 @@ class Customer {
             double thisAmount = 0;
             Rental each = (Rental) enum_rentals.nextElement();
             //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
+            thisAmount = AmountFor(each);
+            // add frequent renter points
+            frequentRenterPoints ++;
+            // add bonus for a two day new release rental
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
+                frequentRenterPoints ++;
+            //show figures for this rental
+            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
+            totalAmount += thisAmount;
+        }
+        //add footer lines
+        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        return result;
+    }
+
+    private double AmountFor(Rental each)
+    {
+        double thisAmount = 0;
+        switch (each.getMovie().getPriceCode()) {
                 case Movie.REGULAR:
                 	thisAmount += 2;
                 	if (each.getDaysRented() > 2)
@@ -42,19 +61,7 @@ class Customer {
                     	thisAmount += (each.getDaysRented() - 3) * 1.5;
                 	break;
             }
-            // add frequent renter points
-            frequentRenterPoints ++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
-                frequentRenterPoints ++;
-            //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
-        }
-        //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
-        return result;
+        return thisAmount;
     }
     
 }
